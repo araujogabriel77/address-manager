0c478b8a79a1a78a4ee0253c8153342efb2091a9
Gabriel Araújo <araujogabrielocn@gmail.com>
2024-09-05 19:27:49 -0300

    chore: adding config service

---
+++ api/src/infra/config/config.type.ts
@@ -0,0 +1,38 @@
+export type AppConfig = {
+  nodeEnv: string;
+  name: string;
+  workingDirectory: string;
+  port: number;
+  apiPrefix: string;
+};
+
+export type AuthConfig = {
+  secret?: string;
+  expires?: string;
+  refreshSecret?: string;
+  refreshExpires?: string;
+};
+
+export type DatabaseConfig = {
+  url?: string;
+  type?: string;
+  host?: string;
+  port?: number;
+  password?: string;
+  name?: string;
+  username?: string;
+  synchronize?: boolean;
+  maxConnections: number;
+  sslEnabled?: boolean;
+  rejectUnauthorized?: boolean;
+  ca?: string;
+  key?: string;
+  cert?: string;
+  endPointId?: string;
+};
+
+export type AllConfigType = {
+  app: AppConfig;
+  auth: AuthConfig;
+  database: DatabaseConfig;
+};
--- api/src/main.ts
+++ api/src/main.ts
@@ -3,10 +3,20 @@ import { AppModule } from './app.module';
 import * as compression from 'compression';
 import helmet from 'helmet';
 import { ClassSerializerInterceptor } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { AllConfigType } from './infra/config/config.type';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService<AllConfigType>);
+
   app.enableCors();
+  app.setGlobalPrefix(
+    configService.getOrThrow('app.apiPrefix', { infer: true }),
+    {
+      exclude: ['/'],
+    },
+  );
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.use(compression());
   app.use(helmet());
