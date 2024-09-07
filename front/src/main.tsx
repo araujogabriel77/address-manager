0c275d1e94754fd2ecbbff64361a415ecc37e825
Gabriel Araújo <araujogabrielocn@gmail.com>
2024-09-06 23:00:08 -0300

    chore: initial client structure and configuration

---
+++ front/src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import {NextUIProvider} from "@nextui-org/react";
+import './index.css'
+import App from './App.tsx';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <NextUIProvider>
+    <main className="dark text-foreground bg-background wrapper">
+      <App />
+    </main>
+  </NextUIProvider>
+  </StrictMode>
+)
